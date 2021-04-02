0498a8b698f7dd2429e259b083aae4e14d40234c
savedev <savedev@example.com>
2021-04-02 12:46:56 -0400

    Update description and URL

--- noQueryParams.user.js
+++ noQueryParams.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         No Query Params
 // @namespace    https://github.com/VoltronicAcid/
-// @downloadURL  https://github.com/VoltronicAcid/noQuery/raw/main/noQuery.user.js
+// @downloadURL  https://github.com/VoltronicAcid/noQueryParams/raw/master/noQueryParams.user.js
 // @version      0.1
-// @description  Remove query parameters from video page URLs
+// @description  Remove query parameters from the URLs of a video's page on SexyPorn
 // @author       VoltronicAcid
 // @match        https://sxyprn.com/post/*.html\?*
 // ==/UserScript==
