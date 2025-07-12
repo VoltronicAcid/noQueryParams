81f6b88483f95fb01d1a4bd50806795bc750623c
VoltronicAcid <78665774+VoltronicAcid@users.noreply.github.com>
2025-07-12 08:29:44 -0400

    icon

--- noQueryParams.user.js
+++ noQueryParams.user.js
@@ -2,10 +2,11 @@
 // @name         SexyPorn - No Query Params
 // @author       VoltronicAcid
 // @description  Remove query parameters from the URL of a video's page on SexyPorn
-// @version      1.0
+// @version      1.1
 // @homepageURL  https://github.com/VoltronicAcid/noQueryParams
 // @supportURL   https://github.com/VoltronicAcid/noQueryParams/issues
 // @downloadURL  https://github.com/VoltronicAcid/noQueryParams/raw/master/noQueryParams.user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=sxyprn.com
 // @match        https://sxyprn.com/post/*.html\?*
 // ==/UserScript==
 
